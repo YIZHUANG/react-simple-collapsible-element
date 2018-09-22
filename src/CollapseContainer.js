f963012c0dd49ccea1a7de38b7e704e2536232a2
YIZHUANG <yi.zhuang1@outlook.com>
2018-09-22 22:38:15 +0300

    2.0.5

--- src/CollapseContainer.js
+++ src/CollapseContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const CollapseContainer = ({ children, currentActiveIndex, onCollapse }) => {
   return React.Children.toArray(children).map((child, index) =>
@@ -7,6 +8,18 @@ const CollapseContainer = ({ children, currentActiveIndex, onCollapse }) => {
       onCollapse: index => onCollapse(index)
     })
   );
+};
+
+CollapseContainer.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.array])
+    .isRequired,
+  currentActiveIndex: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onCollapse: PropTypes.func
+};
+
+CollapseContainer.defaultProps = {
+  currentActiveIndex: undefined,
+  onCollapse: () => {}
 };
 
 export default CollapseContainer;
